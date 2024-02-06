8741bdbf6247ed080dfbe06ffe98eeaeef0437ea
Jesufemi <oladapofemmy@gmail.com>
2024-02-06 22:18:12 +0100

    create base model from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,18 @@ from datetime import datetime
 class BaseModel():
     """A class that defines all common attributes/methods for other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Constructor"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if not args and not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+        else:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key ,value)
     
     def __setattr__(self, name, value):
         """set attribute magic method"""
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A python module for testing BaseModel"""
+
+
+from models.base_model import BaseModel
+import unittest
+
+class TestBaseModel(unittest.TestCase):
+    """A test for the BaseModel Class"""
+
