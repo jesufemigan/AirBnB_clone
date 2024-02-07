7c0f604e240fbdfeb916804f1e23f35cfb62aab0
Jesufemi <oladapofemmy@gmail.com>
2024-02-07 02:35:05 +0100

    set up console

--- console.py
+++ console.py
@@ -7,13 +7,21 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     """A console for HBNB"""
-    def do_quit(self):
+    prompt = "(hbnb) "
+
+    def emptyline(self):
+        """do nothing for empty line"""
+        pass
+
+    def do_quit(self, line):
+        """Quit command to exit the program
+        """
+        return True
+
+    def do_EOF(self, line):
         """exits the program"""
         return True
-    def do_EOF(self):
-        """exits the program"""
-        return True
 
 
-if __name == "__main__":
-
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
