0b6acfa879d03ca20911cee98a4c0e15f2da1441
Jesufemi <oladapofemmy@gmail.com>
2024-02-12 23:39:21 +0100

    test_console

--- tests/test_console.py
+++ tests/test_console.py
@@ -3,11 +3,23 @@
 
 
 import unittest
+from console import HBNBCommand
+from unittest.mock import patch
+from io import StringIO
 
 
 class TestConsole(unittest.TestCase):
     """Tests for the console"""
-    pass
+
+    def test_1_prompt(self):
+        """Test the prompt"""
+        self.assertTrue("(hbnb) ", HBNBCommand.prompt)
+
+    def test_2_empty_line(self):
+        """Test empty line"""
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd(""))
+            self.assertEqual("", output.getvalue().strip())
 
     if __name__ == "__main__":
         unittest.main()
